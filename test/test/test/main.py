824cdf7a345e5e30f97e7c8e8f77d02b35e3c3dd
gdal@thinkpad <gdal@cs.ru.nl>
2018-02-10 01:48:21 +0100

    fixed main

--- test/test/test/main.py
+++ test/test/test/main.py
@@ -12,32 +12,27 @@ def process(options):
         list_bayesian_networks()
         sys.exit(0)
     else:
-
         if options.test != None:
             if options.test == "inference":
-                compare_inference(options)
+                inference(options)
             elif options.test == "compilation":
-                compare_compilation(options)
+                compilation(options)
             else:
                 encoding(options)
+
 
 def main():
     parser = argparse.ArgumentParser(description='WMC testing suite',add_help=False)
 
     parser.add_argument('--help',action='help',help='Show this help message and exit')
 
-    group = parser.add_mutually_exclusive_group(required=False)
     test_options = ["compilation","inference","encoding"]
-    group.add_argument('--test',dest='test',nargs=1,choices=test_options, help='Choose what to test. Choices are ' + ', '.join(test_options), required=False,metavar='TEST')
-
-    #dataset_options = ["ijcai"]
-    #parser.add_argument('-d','--dataset',nargs=1,choices=dataset_options, help='Dataset used for testing', required=False, default='ijcai')
+    parser.add_argument('--test',dest='test',nargs=1,choices=test_options, help='Choose what to test. Choices are ' + ', '.join(test_options), required=False,metavar='TEST')
 
     bdd_options = ["wpbdd","parallel-wpbdd","pwpbdd","parallel-pwpbdd","sdd","rsdd","obdd"]
-    parser.add_argument('--bdd',dest='bdds',nargs='+', help='Type of decision diagram. Choices are ' + ', '.join(bdd_options), choices=bdd_options,required=False,default=None,metavar='BDD')
-    group = parser.add_mutually_exclusive_group(required=False)
-    parser.add_argument('--network',dest='network',nargs='+', help='Path(s) or basename(s) of HUGIN files')
-    parser.add_argument('--list', dest='list', action='store_true', help='Print a list of available Bayesian networks', required=False)
+    parser.add_argument('--bdd',dest='bdd',nargs='+', help='Type of BDD. Choices are ' + ', '.join(bdd_options), choices=bdd_options,required=False,default=None,metavar='BDD')
+    parser.add_argument('--network',dest='network',nargs='+', help='Bayesian network(s) used for testing')
+    parser.add_argument('--list', dest='list', action='store_true', help='Print list of available Bayesian networks', required=False)
 
     if len(sys.argv)==1:
         parser.print_help()
